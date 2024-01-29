47624090d4c07af2e6d3ff57b560cac28452c96f
PoornimaKatti <153407034+PoornimaKatti@users.noreply.github.com>
2024-01-29 09:31:53 +0530

    Add files via upload

---
+++ JavaPractice/src/com/JavaPratice/AddFloatValue.java
@@ -0,0 +1,17 @@
+package com.JavaPratice;
+
+public class AddFloatValue {
+        public static void main(String[] args) {
+            double a = 0.1;
+            double b = 0.2;
+
+            double sum = a + b;
+
+            System.out.println("a: " + a);
+            System.out.println("b: " + b);
+            System.out.println("Sum: " + sum);
+
+            boolean isEqual = sum == 0.3;
+            System.out.println("Is sum equal to 0.3? " + isEqual);
+        }
+    }
