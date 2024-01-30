6085b782c2d53afe2afbde58990b22d4cd2adffd
PoornimaKatti <153407034+PoornimaKatti@users.noreply.github.com>
2024-01-30 08:24:29 +0530

    Add files via upload

---
+++ SwapNumbers.java
@@ -0,0 +1,17 @@
+package com.JavaPratice;
+//swap 2 number using 3rd variable
+import java.util.*;
+public class SwapNumbers {
+    public static void main(String[] args) {
+        int x,y,temp;
+        System.out.println("Enter x and y:");
+        Scanner in = new Scanner(System.in);
+        x=in.nextInt();
+        y=in.nextInt();
+        System.out.println("Before Swapping\nx="+x+"\ny="+y);
+        temp=x;
+        x=y;
+        y=temp;
+        System.out.println("After Swapping\nx="+x+"\ny="+y);
+    }
+}
