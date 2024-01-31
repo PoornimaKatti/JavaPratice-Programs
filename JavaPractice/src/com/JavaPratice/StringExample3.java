5e2351c51abca0a172cbe0d9961b81191983aa86
PoornimaKatti <153407034+PoornimaKatti@users.noreply.github.com>
2024-01-31 10:55:59 +0530

    Add files via upload

---
+++ JavaPractice/src/com/JavaPratice/StringExample3.java
@@ -0,0 +1,9 @@
+package com.JavaPratice;
+//Substring example
+public class StringExample3 {
+    public static void main(String[] args) {
+        String sentence = "Java is fun!";
+        boolean containsJava = sentence.contains("Java");
+        System.out.println("Contains 'Java': " + containsJava);
+    }
+}
