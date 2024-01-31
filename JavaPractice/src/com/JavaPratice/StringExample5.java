015334e2f9e75b118c8620fac4aad46e52a88d05
PoornimaKatti <153407034+PoornimaKatti@users.noreply.github.com>
2024-01-31 10:57:02 +0530

    Add files via upload

---
+++ JavaPractice/src/com/JavaPratice/StringExample5.java
@@ -0,0 +1,10 @@
+package com.JavaPratice;
+//StringBuilder for Mutable Operations:
+public class StringExample5 {
+    public static void main(String[] args) {
+        StringBuilder mutableText = new StringBuilder("Hello");
+        mutableText.append(" World");
+        System.out.println(mutableText.toString());
+    }
+
+}
