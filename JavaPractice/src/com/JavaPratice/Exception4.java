20aa8adbfec432f54bbdf9407d2da96bb9f317ad
PoornimaKatti <153407034+PoornimaKatti@users.noreply.github.com>
2024-02-01 18:25:29 +0530

    Add files via upload

---
+++ JavaPractice/src/com/JavaPratice/Exception4.java
@@ -0,0 +1,19 @@
+package com.JavaPratice;
+class Person{
+    public void talk() {}
+}
+public class Exception4{
+    public static void main(String args[]){
+        Person p = null;
+        try{
+            p.talk();
+        }
+        catch(NullPointerException e){
+            System.out.print("There is a NullPointerException. ");
+        }
+        catch(Exception e){
+            System.out.print("There is an Exception. ");
+        }
+        System.out.print("Everything went fine. ");
+    }
+}
