12964f543387b1bc0e56f8a46ea7c92dcbf2548a
PoornimaKatti <153407034+PoornimaKatti@users.noreply.github.com>
2024-02-05 13:49:55 +0530

    Add files via upload

---
+++ JavaPractice/src/com/JavaPratice/pattern5.java
@@ -0,0 +1,23 @@
+package com.JavaPratice;
+
+import java.util.Scanner;
+
+public class pattern5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Number of rows you want in this pattern:");
+        int rows = scanner.nextInt();
+        System.out.println("----Pattern is----");
+        for (int i = 1; i <= rows; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                System.out.print(j+" ");
+            }
+
+            System.out.println();
+        }
+
+        scanner.close();
+    }
+}
