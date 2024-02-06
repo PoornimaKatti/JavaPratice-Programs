add9cb971f093199464d7c9c47dd2d41e7e304f9
PoornimaKatti <153407034+PoornimaKatti@users.noreply.github.com>
2024-02-06 17:30:57 +0530

    Add files via upload

---
+++ JavaPractice/src/com/JavaPratice/ArrayListExample2.java
@@ -0,0 +1,16 @@
+package com.JavaPratice;
+
+import java.util.ArrayList;
+
+public class ArrayListExample2 {
+
+        public static void main(String[] args) {
+            ArrayList<String> cars = new ArrayList<String>();
+            cars.add("Volvo");
+            cars.add("BMW");
+            cars.add("Ford");
+            cars.add("Mazda");
+            System.out.println(cars);
+        }
+    }
+
