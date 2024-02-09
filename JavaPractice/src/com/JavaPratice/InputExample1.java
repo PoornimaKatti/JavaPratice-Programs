6bb9a54212a887e8700fddc7cd26d2df617602cb
PoornimaKatti <153407034+PoornimaKatti@users.noreply.github.com>
2024-02-09 09:59:45 +0530

    Add files via upload

---
+++ JavaPractice/src/com/JavaPratice/InputExample1.java
@@ -0,0 +1,23 @@
+package com.JavaPratice;
+/*
+Create sqr and cube methods that take an integer (int) and return its square and cube, respectively.
+
+Methods must be public and static, with an int return type.
+
+Method main is not involved in testing
+ */
+public class InputExample1 {
+
+        public static int sqr(int num) {
+            return num * num;
+        }
+
+        public static int cube(int num) {
+            return num * num * num;
+        }
+    }
+
+
+
+
+
