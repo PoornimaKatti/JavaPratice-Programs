9e0ac132026b80ae54dfcd0ad16165b744cc7976
PoornimaKatti <153407034+PoornimaKatti@users.noreply.github.com>
2024-02-13 16:24:10 +0530

    Add files via upload

---
+++ JavaPractice/src/com/JavaPratice/OperatorsExample3.java
@@ -0,0 +1,9 @@
+package com.JavaPratice;
+//Multiplication Example
+public class OperatorsExample3 {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 3;
+        System.out.println(x * y);
+    }
+}
