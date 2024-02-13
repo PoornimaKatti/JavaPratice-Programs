e47b80ecd70b80384126023af7af8e0e6889efe7
PoornimaKatti <153407034+PoornimaKatti@users.noreply.github.com>
2024-02-13 16:24:00 +0530

    Add files via upload

---
+++ JavaPractice/src/com/JavaPratice/OperatorsExample2.java
@@ -0,0 +1,9 @@
+package com.JavaPratice;
+//Subtraction Operator
+public class OperatorsExample2 {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 3;
+        System.out.println(x - y);
+    }
+}
