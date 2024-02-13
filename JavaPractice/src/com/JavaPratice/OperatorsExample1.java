b64ae198d033157448e8abc5c69fd6ec88738911
PoornimaKatti <153407034+PoornimaKatti@users.noreply.github.com>
2024-02-13 16:23:52 +0530

    Add files via upload

---
+++ JavaPractice/src/com/JavaPratice/OperatorsExample1.java
@@ -0,0 +1,9 @@
+package com.JavaPratice;
+//Addition operator
+public class OperatorsExample1 {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 3;
+        System.out.println(x + y);
+    }
+}
