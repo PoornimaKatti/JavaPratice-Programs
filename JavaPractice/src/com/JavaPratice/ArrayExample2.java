cec4d787a203ed5453c648f1522a1c7cd7c5b483
PoornimaKatti <153407034+PoornimaKatti@users.noreply.github.com>
2024-02-15 20:08:46 +0530

    Add files via upload

---
+++ JavaPractice/src/com/JavaPratice/ArrayExample2.java
@@ -0,0 +1,13 @@
+package com.JavaPratice;
+//Declaration, Instantiation and Initialization of Java Array
+public class ArrayExample2 {
+    //Java Program to illustrate the use of declaration, instantiation
+//and initialization of Java array in a single line
+
+        public static void main(String args[]){
+            int a[]={33,3,4,5};//declaration, instantiation and initialization
+//printing array
+            for(int i=0;i<a.length;i++)//length is the property of array
+                System.out.println(a[i]);
+        }}
+
