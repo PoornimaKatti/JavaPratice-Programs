cef88fe7ab50317882071b42dd4b7cae6c4ed2e2
PoornimaKatti <153407034+PoornimaKatti@users.noreply.github.com>
2024-03-06 16:07:50 +0530

    Add files via upload

---
+++ JavaPractice/src/com/JavaPratice/CopyArray4.java
@@ -0,0 +1,30 @@
+package com.JavaPratice;
+
+import java.util.Arrays;
+
+public class CopyArray4 {
+
+        public static void main(String[] args)
+        {
+            // Custom input array
+            int a[] = { 1, 8, 3, 5, 9, 10 };
+
+            // Creating an array b[] and
+            // copying elements of a[] to b[]
+            int b[] = Arrays.copyOfRange(a, 2, 6);
+
+            // Changing b[] to verify that
+            // b[] is different from a[]
+
+            // Iterating over array a[]
+            System.out.println("Contents of a[] ");
+            for (int i = 0; i < a.length; i++)
+                System.out.print(a[i] + " ");
+
+            // Iterating over array b[]
+            System.out.println("\n\nContents of b[] ");
+            for (int i = 0; i < b.length; i++)
+                System.out.print(b[i] + " ");
+        }
+    }
+
