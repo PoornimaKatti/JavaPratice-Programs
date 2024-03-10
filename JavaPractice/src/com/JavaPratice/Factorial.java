2bdbaaf74f4880e1fdd9ceb9fc80e2f2028bcded
PoornimaKatti <153407034+PoornimaKatti@users.noreply.github.com>
2024-03-10 13:08:14 +0530

    Add files via upload

---
+++ JavaPractice/src/com/JavaPratice/Factorial.java
@@ -0,0 +1,23 @@
+package com.JavaPratice;
+
+import java.util.Scanner;
+// FactorialWithLoop
+public class Factorial {
+
+
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter a number: ");
+            int num = scanner.nextInt();
+            int factorial = 1;
+
+            for (int i = 1; i <= num; i++) {
+                factorial *= i;
+            }
+
+            System.out.println("Factorial of " + num + " using loop: " + factorial);
+            scanner.close();
+        }
+    }
+
+
