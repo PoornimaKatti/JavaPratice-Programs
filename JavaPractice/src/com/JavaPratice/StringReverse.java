e073dc9ad24abc79280ea53421125c501bc0da3f
PoornimaKatti <153407034+PoornimaKatti@users.noreply.github.com>
2024-03-13 14:28:08 +0530

    Add files via upload

---
+++ JavaPractice/src/com/JavaPratice/StringReverse.java
@@ -0,0 +1,9 @@
+package com.JavaPratice;
+
+public class StringReverse {
+    public static void main(String[] args) {
+        String str = "Hello World";
+        StringBuilder sb = new StringBuilder(str);
+        System.out.println(sb.reverse().toString());
+    }
+}
