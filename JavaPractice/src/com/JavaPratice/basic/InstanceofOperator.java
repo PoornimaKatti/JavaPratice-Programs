02f7a5c6e6299effde548fb917f2d2cda00aa71e
PoornimaKatti <153407034+PoornimaKatti@users.noreply.github.com>
2024-03-22 17:09:23 +0530

    Add files via upload

---
+++ JavaPractice/src/com/JavaPratice/basic/InstanceofOperator.java
@@ -0,0 +1,13 @@
+package com.JavaPratice;
+
+public class InstanceofOperator {
+    
+        public static void main(String[] args) {
+            Integer i = null;
+            Integer j = 10;
+
+            System.out.println(i instanceof Integer);
+            System.out.println(j instanceof Integer);
+        }
+    }
+
