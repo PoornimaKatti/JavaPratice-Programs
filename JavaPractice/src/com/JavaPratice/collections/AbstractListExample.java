0ff656be556b237a345288fe5a6b9e0c93d2cbdc
PoornimaKatti <153407034+PoornimaKatti@users.noreply.github.com>
2024-03-27 14:32:54 +0530

    Add files via upload

---
+++ JavaPractice/src/com/JavaPratice/collections/AbstractListExample.java
@@ -0,0 +1,18 @@
+package com.JavaPratice;
+import java.util.*;
+public class AbstractListExample {
+
+    public static void main(String[] args) {
+        AbstractList<String> list = new LinkedList<String>();
+
+        // Using add() method to add elements in the list
+        list.add("Apple");
+        list.add("Banana");
+        list.add("Orange");
+
+        System.out.println("Abstract List: "+list);
+
+        list.remove(2);
+        System.out.println("Final AbstractList: " + list);
+    }
+}
